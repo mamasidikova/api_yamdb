82abf90941cfedee92ba9e8ead82b0ec1438a4af
Наталия Чернякова <chvv.n@yandex.ru>
2022-05-31 22:23:23 +0300

    модели и сериализаторы в общих чертах

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title, GenreTitle, Review, Comment,
+
+from django.db.models import Avg
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
+
+
+class ReadOnlyTitleSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField()
+    genre = GenreSerializer(
+        read_only=True,
+        many=True)
+    category = CategorySerializer(
+        read_only=True,
+        many=False
+    )
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
+
+    def get_rating(self, obj):
+        all_reviews = obj.reviews.all()
+        avg_rating = all_reviews.aggregate(Avg('score'))
+        if 
+        return 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    pass
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,84 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Категория',
+        max_length=256
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True)
+
+    def __str__(self):
+        self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name='Жанр',
+        max_length=256
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True)
+
+    def __str__(self):
+        self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=100
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска',
+        max_length=4
+    )
+    # валидация year: в модели или сериализаторе
+    rating = models.IntegerField(
+        verbose_name='Рейтинг на основе отзывов',
+        default=None
+    )
+    description = models.CharField(
+        verbose_name='Описание',
+        max_length=250,
+        null=True,
+        blank=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр(жанры)',
+        on_delete=models.CASCADE
+    )
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} относится к жанру(жанрам): {self.genre}'
+
+
+class Review(models.Model):
     pass
 
 
-
+class Comment(models.Model):
+    pass
