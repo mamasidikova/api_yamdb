1d1a36023e5e7f93e8aa1b62e360b8f08e3018a7
Наталия Чернякова <chvv.n@yandex.ru>
2022-06-03 12:48:36 +0300

    IsAdminSuperuserOrReadOnly измененеа на IsAdminOrReadOnly, миксин вынесен в отдельный файл, отредактированы вьюсеты.

---
+++ api_yamdb/api/mixins.py
@@ -0,0 +1,10 @@
+from rest_framework import mixins, viewsets
+
+
+class GetPostDeleteViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    pass
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,45 +1,33 @@
 from reviews.models import Category, Genre, Title
 from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-# from rest_framework.permissions import (IsAuthenticated,
-#                                         IsAuthenticatedOrReadOnly)
 from django_filters.rest_framework import DjangoFilterBackend
+from .mixins import GetPostDeleteViewSet
 
-from rest_framework import mixins
-
-from .permissions import IsAdminSuperuserOrReadOnly
+from .permissions import IsAdminOrReadOnly
 from .serializers import (CategorySerializer, GenreSerializer, TitleSerializer,
                           ReadOnlyTitleSerializer)
 from .filters import TitleFilter
 
 
-class GetPostDeleteViewSet(
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin,
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-):
-    pass
-
-
 class CategoryViewSet(GetPostDeleteViewSet):
+    lookup_field = "slug"
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAdminSuperuserOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     pagination_class = LimitOffsetPagination
-    filter_backends = (filters.SearchFilter)
-    search_fields = ('name',)
-    lookup_field = "slug"
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', ]
 
 
 class GenreViewSet(GetPostDeleteViewSet):
+    lookup_field = "slug"
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [IsAdminSuperuserOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     pagination_class = LimitOffsetPagination
-    filter_backends = (filters.SearchFilter)
-    search_fields = ('name',)
-    lookup_field = "slug"
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', ]
 
 
 class TitleViewSet(viewsets.ModelViewSet):
@@ -47,17 +35,13 @@ class TitleViewSet(viewsets.ModelViewSet):
     serializer_class = TitleSerializer
     pagination_class = LimitOffsetPagination
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
-    permission_classes = [IsAdminSuperuserOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     filter_backends = (DjangoFilterBackend,)
-    # filterset_fields = ('category', 'genre', 'name', 'year',)
     filterset_class = TitleFilter
 
     def get_serializer_class(self):
-        # Если запрошенное действие (action) — получение списка объектов ('list')
         if self.action in ('list', 'retrieve'):
-            # ...то применяем CatListSerializer
             return ReadOnlyTitleSerializer
-        # А если запрошенное действие — не 'list', применяем CatSerializer
         return TitleSerializer
 
 
